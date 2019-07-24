7d180a4e378cd8bebe4c5c8fd3c99e708c19a79d
Josh Latour <jlatour88@gmail.com>
2019-07-24 14:33:56 -0500

    added css, js links

---
+++ app/public/assets/main.js
@@ -0,0 +1,23 @@
+$('#submit').on('click', function(event) {
+   event.preventDefault();
+
+   var scoreRaw = $('.survey-score')
+      .map(function() {
+         return parseInt($(this).val());
+      })
+      .get();
+
+      console.log('scoreRaw: '+ scoreRaw);
+   var scores = scoreRaw.map(Number);
+
+   // Here we grab the form elements
+   var newFriend = {
+   name: $('#survey-name').val().trim(),
+   photo: $('#survey-photo').val().trim(),
+   scores: scores
+   };
+
+   console.log(newFriend);
+});
+
+// $.post('/api/friends', newFriend);
