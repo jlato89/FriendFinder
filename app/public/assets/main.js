b81b3bcb1ec1a98b5ca924e79cd8cf767ee795fc
Josh Latour <jlatour88@gmail.com>
2019-07-25 14:54:35 -0500

    changed validation and added post

--- app/public/assets/main.js
+++ app/public/assets/main.js
@@ -1,27 +1,35 @@
 $('#submit').on('click', function(event) {
    event.preventDefault();
 
+   // User validation check for missing fields
+   var nameEmpty =
+      $('#survey-name').filter(function() {
+         return $.trim(this.value).length === 0;
+      }).length > 0;
+
+   var photoEmpty =
+      $('#survey-photo').filter(function() {
+         return $.trim(this.value).length === 0;
+      }).length > 0;
+
    var questionEmpty =
       $('[name="score"]').filter(function() {
          return $.trim(this.value).length === 0;
-      }).length > 0;
+      }).length > 0;   
+   
 
-   var inputEmpty =
-      $(':input').filter(function() {
-         return $.trim(this.value).length === 0;
-      }).length > 0;
-
-   if (questionEmpty || inputEmpty) {
+      console.log('name: '+ nameEmpty);
+      console.log('photo: '+ photoEmpty);
+      console.log('questions: '+ questionEmpty);
+      
+   if (nameEmpty || photoEmpty || questionEmpty) {
       console.log('Please fill out all questions.');
    } else {
       var scores = $('[name="score"]')
          .map(function() {
-            return $(this).val();
+            return parseInt($(this).val());
          })
          .get();
-
-      // console.log('scoreRaw: ' + scoreRaw);
-      // var scores = scoreRaw.map(Number);
 
       // Here we grab the form elements
       var newFriend = {
@@ -36,6 +44,6 @@ $('#submit').on('click', function(event) {
 
       console.log(newFriend);
 
-      // $.post('/api/friends', newFriend);
+      $.post('/api/friends', newFriend);
    }
 });
