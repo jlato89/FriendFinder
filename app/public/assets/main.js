1c1f34113520d580cf8556affebd5ef6b02a58ce
Josh Latour <jlatour88@gmail.com>
2019-07-26 00:13:02 -0500

    moved form validation to api/friends. set temp alert for error with form(not working currently)

--- app/public/assets/main.js
+++ app/public/assets/main.js
@@ -1,33 +1,9 @@
 $('#submit').on('click', function(event) {
    event.preventDefault();
 
-   // User validation check for missing fields
-   var nameEmpty =
-      $('#survey-name').filter(function() {
-         return $.trim(this.value).length === 0;
-      }).length > 0;
-
-   var photoEmpty =
-      $('#survey-photo').filter(function() {
-         return $.trim(this.value).length === 0;
-      }).length > 0;
-
-   var questionEmpty =
-      $('[name="score"]').filter(function() {
-         return $.trim(this.value).length === 0;
-      }).length > 0;   
-   
-
-      console.log('name: '+ nameEmpty);
-      console.log('photo: '+ photoEmpty);
-      console.log('questions: '+ questionEmpty);
-      
-   if (nameEmpty || photoEmpty || questionEmpty) {
-      console.log('Please fill out all questions.');
-   } else {
       var scores = $('[name="score"]')
          .map(function() {
-            return parseInt($(this).val());
+            return $(this).val();
          })
          .get();
 
@@ -44,6 +20,9 @@ $('#submit').on('click', function(event) {
 
       console.log(newFriend);
 
-      $.post('/api/friends', newFriend);
-   }
+      $.post('/api/friends', newFriend, function(data) {
+         if (!data) {
+            alert('Please fill out the entire form'); //! change this to a modal msg
+         }
+      });
 });
