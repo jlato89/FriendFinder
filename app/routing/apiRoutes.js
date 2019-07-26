4a67196912a4fa16257b3411d04b420e952f0479
Josh Latour <jlatour88@gmail.com>
2019-07-26 00:11:39 -0500

    removed some spaces and added missing semicolon

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,15 @@
 var friends = require('../data/friends');
 
-
 module.exports = function(app) {
-
    app.get('/api/friends', function(req, res) {
       res.json(friends);
    });
 
    app.post('/api/friends', function(req, res) {
       var scores = req.body.scores.map(Number);
-      
-      if (!req.body.name || !req.body.photo  || scores.length < 10) {
-         return res.status(400).json({ msg: 'Please fill out entire form'});
+
+      if (!req.body.name || !req.body.photo || scores.length < 10) {
+         return res.status(400).json({ msg: 'Please fill out entire form' });
          // res.json(false); //! unable to send false so main.js can run callback code. Need to find out why.
       }
 
@@ -19,8 +17,8 @@ module.exports = function(app) {
          name: req.body.name,
          photo: req.body.photo,
          scores: scores
-      }
-      
+      };
+
       friends.push(newFriend);
       res.json(friends);
    });
