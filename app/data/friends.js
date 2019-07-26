b2b1fe9b3a16533a0e333ea02664fe7f76c795b6
Josh Latour <jlatour88@gmail.com>
2019-07-26 10:28:08 -0500

    added another test friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,13 @@
 var friendsData = [
    {
-      'name': 'Josh',
-      'photo': 'https://picsum.photos/200',
-      'scores': [
-         5,
-         1,
-         4,
-         4,
-         5,
-         1,
-         2,
-         5,
-         4,
-         1
-      ]
+      name: 'Test 1',
+      photo: 'https://picsum.photos/200',
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+   },
+   {
+      name: 'Test 2',
+      photo: 'https://picsum.photos/200',
+      scores: [2, 1, 1, 4, 5, 1, 1, 5, 4, 5]
    }
 ];
 
