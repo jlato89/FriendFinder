bf3483b9548343f82fd2bec03787eca54a0207c1
Josh Latour <jlatour88@gmail.com>
2019-07-26 00:05:15 -0500

    got api data posting to local variable correctly. validation for scores not working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,27 @@ module.exports = function(app) {
    });
 
    app.post('/api/friends', function(req, res) {
-      friends.push(req.body);
-      console.log(req.body);
+      var scores = req.body.scores.map(Number);
+
+      // Check to see if all questions were answered
+      var scoresNull = scores.some(function(val) {
+         return val === null;
+      });
+      console.log('scores: '+ scores);
+      console.log('scoreNull: '+scoresNull);
+      
+      if (!req.body.name || !req.body.photo  || scoresNull) {
+         return res.status(400).json({ msg: 'Please fill out entire form'});
+         // res.json(false); //! unable to send false so main.js can run callback code. Need to find out why.
+      } else {
+         var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: scores
+         }
+         
+         friends.push(newFriend);
+         res.json(friends);
+      }
    });
 };
