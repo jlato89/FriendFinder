77aecf2f1113f40e8bfee0d7ee1fcbfc9ce17668
jlatour88 <jlatour88@gmail.com>
2019-07-28 11:18:57 -0500

    just updated and added some test friends into array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,23 @@
 var friendsData = [
    {
-      name: 'Test 1',
+      name: 'Josh Latour',
       photo: 'https://picsum.photos/200',
       scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
    },
    {
-      name: 'Test 2',
+      name: 'Brad Pain',
       photo: 'https://picsum.photos/200',
       scores: [2, 1, 1, 4, 5, 1, 1, 5, 4, 5]
+   },
+   {
+      name: 'Brad Pain',
+      photo: 'https://picsum.photos/200',
+      scores: [3, 4, 1, 3, 3, 5, 3, 5, 2, 5]
+   },
+   {
+      name: 'Samantha Gales',
+      photo: 'https://picsum.photos/200',
+      scores: [1, 2, 2, 4, 4, 1, 5, 5, 4, 5]
    }
 ];
 
